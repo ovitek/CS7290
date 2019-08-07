2fad205b1ba7b3ae77255f85d2ecb26e51aeea0d
ovitek <o.vitek@neu.edu>
2019-08-06 21:07:24 -0400

    1

--- F19/build_site.R
+++ F19/build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/olgavitek/Documents/GitHub/CS7290/S18")
+setwd("/Users/olgavitek/Documents/GitHub/CS7290/F19")
 
 #render your sweet site. 
 rmarkdown::render_site()
