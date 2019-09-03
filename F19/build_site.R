a7688d94ef0751697e380b4a219b1122bf8f10ca
Olga Vitek <o.vitek@neu.edu>
2019-09-02 22:18:18 -0400

    1

--- F19/build_site.R
+++ F19/build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/olgavitek/Documents/GitHub/CS7290/F19")
+setwd("/Users/olga/Documents/GitHub/CS7290/F19")
 
 #render your sweet site. 
 rmarkdown::render_site()
